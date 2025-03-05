e662320a67ead5694114b2f485c7aaf68b9855dd
mel-ak <melakesisay@gmail.com>
2025-03-05 14:50:44 +0300

    testing docker

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Docker latest! and check");
+  res.send("Hello, Docker latest! sorry this is erroe");
 });
 
 app.listen(PORT, () => {
